185c1fef2237baf53a763f2bf9987129f99b4008
MahmoudElbedewy <kahrabam706@gmail.com>
2026-07-28 18:00:11 +0300

    edit theme

--- vite.config.ts
+++ vite.config.ts
@@ -17,5 +17,17 @@ export default defineConfig(({ mode }) => {
         },
       },
     },
+    build: {
+      chunkSizeWarningLimit: 1500,
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          }
+        }
+      }
+    },
   }
 })
